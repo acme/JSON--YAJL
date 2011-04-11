215af62522bcf7362f90b4bdbd9ade74ab183007
Leon Brocard <acme@astray.com>
2011-04-11 11:42:01 +0200

    Add some documentation

--- lib/JSON/YAJL.pm
+++ lib/JSON/YAJL.pm
@@ -5,3 +5,36 @@ use JSON::YAJL::Generator;
 our $VERSION = '0.01';
 
 1;
+
+=head1 NAME
+
+JSON::YAJL - An interface to the YAJL JSON parsing and generation library
+
+=head1 SYNOPSIS
+
+  use JSON::YAJL;
+  # see JSON::YAJL::Generator
+
+=head1 DESCRIPTION
+
+YAJL is Yet Another JSON Library. YAJL is a small event-driven (SAX-style) JSON
+parser written in ANSI C, and a small validating JSON generator, by Lloyd
+Hilaiel This module is a Perl interface to that library.
+
+At the moment this only wraps the generation library, see
+L<JSON::YAJL::Generator>. Patches for parsing welcome.
+
+This is a very early release to see how cross-platform the underlying code is.
+The API may change in future.
+
+=head1 AUTHOR
+
+Leon Brocard <acme@astray.com>
+
+=head1 LICENSE
+
+This module is free software; you can redistribute it or modify it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<JSON::YAJL::Generator>
--- lib/JSON/YAJL/Generator.pm
+++ lib/JSON/YAJL/Generator.pm
@@ -7,3 +7,88 @@ require XSLoader;
 XSLoader::load( 'JSON::YAJL::Generator', $VERSION );
 
 1;
+
+=head1 NAME
+
+JSON::YAJL::Generator - JSON generation with YAJL
+
+=head1 SYNOPSIS
+
+  use JSON::YAJL::Generator;
+  my $yajl = JSON::YAJL::Generator->new();
+  # or to beautify (indent):
+  #  my $yajl = JSON::YAJL::Generator->new( 1, '    ' );
+  $yajl->map_open();
+  $yajl->string("integer");
+  $yajl->integer(123);
+  $yajl->string("double");
+  $yajl->double("1.23");
+  $yajl->string("number");
+  $yajl->number("3.141");
+  $yajl->string("string");
+  $yajl->string("a string");
+  $yajl->string("string2");
+  $yajl->string("another string");
+  $yajl->string("null");
+  $yajl->null();
+  $yajl->string("true");
+  $yajl->bool(1);
+  $yajl->string("false");
+  $yajl->bool(0);
+  $yajl->string("map");
+  $yajl->map_open();
+  $yajl->string("key");
+  $yajl->string("value");
+  $yajl->string("array");
+  $yajl->array_open();
+  $yajl->integer(1);
+  $yajl->integer(2);
+  $yajl->integer(3);
+  $yajl->array_close();
+  $yajl->map_close();
+  $yajl->map_close();
+  print $yajl->get_buf;
+  $yajl->clear;
+
+  # This prints non-beautified:
+  {"integer":123,"double":1.2299999999999999822,"number":3.141,"string":"a string","string2":"another string","null":null,"true":true,"false":false,"map":{"key":"value","array":[1,2,3]}}
+  # or beautified:
+  {
+     "integer": 123,
+     "double": 1.2299999999999999822,
+     "number": 3.141,
+     "string": "a string",
+     "string2": "another string",
+     "null": null,
+     "true": true,
+     "false": false,
+     "map": {
+        "key": "value",
+        "array": [
+           1,
+           2,
+           3
+        ]
+     }
+  }
+
+=head1 DESCRIPTION
+
+This module allows you to generator JSON with YAJL. This is quite a low-level
+interface for generating JSON and it accumulates JSON in an internal buffer
+until you fetch it.
+
+This is a very early release to see how cross-platform the underlying code is.
+The API may change in future.
+
+=head1 AUTHOR
+
+Leon Brocard <acme@astray.com>
+
+=head1 LICENSE
+
+This module is free software; you can redistribute it or modify it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<JSON::YAJL>
