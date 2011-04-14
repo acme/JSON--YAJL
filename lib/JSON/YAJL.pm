ca63bc91cbbbd9f261e468bf73ca7d1b8886d198
Leon Brocard <acme@astray.com>
2011-04-14 10:27:51 +0200

    And release 0.05

--- lib/JSON/YAJL.pm
+++ lib/JSON/YAJL.pm
@@ -2,7 +2,7 @@ package JSON::YAJL;
 use strict;
 use warnings;
 use JSON::YAJL::Generator;
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 1;
 
--- lib/JSON/YAJL/Generator.pm
+++ lib/JSON/YAJL/Generator.pm
@@ -1,7 +1,7 @@
 package JSON::YAJL::Generator;
 use strict;
 use warnings;
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 require XSLoader;
 XSLoader::load( 'JSON::YAJL::Generator', $VERSION );
