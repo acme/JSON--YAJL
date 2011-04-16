0f02723a965c5c182b73cb8054b6daff170c8fa7
Leon Brocard <acme@astray.com>
2011-04-16 19:20:16 +0200

    Add documentation on how to use the parser

--- lib/JSON/YAJL/Parser.pm
+++ lib/JSON/YAJL/Parser.pm
@@ -14,11 +14,61 @@ JSON::YAJL::Parser - JSON parsing with YAJL
 
 =head1 SYNOPSIS
 
-  use JSON::YAJL::Generator;
+  use JSON::YAJL;
+  my $text;
+  my $yajl_callbacks = JSON::YAJL::Parser->new(
+      0, 0,
+      [   sub { $text .= "null\n" },
+          sub { $text .= "bool: @_\n" },
+          undef,
+          undef,
+          sub { $text .= "number: @_\n" },
+          sub { $text .= "string: @_\n" },
+          sub { $text .= "map_open\n" },
+          sub { $text .= "map_key: @_\n" },
+          sub { $text .= "map_close\n" },
+          sub { $text .= "array_open\n" },
+          sub { $text .= "array_close\n" },
+      ]
+  );
+  my $json
+      = '{"integer":123,"double":4,"number":3.141,"string":"a string","string2":"another string","null":null,"true":true,"false":false,"map":{"key":"value","array":[1,2,3]}}';
+  $yajl_callbacks->parse($json);
+  $yajl_callbacks->parse_complete();
+  # $text is now:
+  # map_open
+  # map_key: integer
+  # number: 123
+  # map_key: double
+  # number: 4
+  # map_key: number
+  # number: 3.141
+  # map_key: string
+  # string: a string
+  # map_key: string2
+  # string: another string
+  # map_key: null
+  # null
+  # map_key: true
+  # bool: 1
+  # map_key: false
+  # bool: 0
+  # map_key: map
+  # map_open
+  # map_key: key
+  # string: value
+  # map_key: array
+  # array_open
+  # number: 1
+  # number: 2
+  # number: 3
+  # array_close
+  # map_close
+  # map_close
 
 =head1 DESCRIPTION
 
-This module allows you to parser JSON with YAJL. This is quite a low-level
+This module allows you to parse JSON with YAJL. This is quite a low-level
 interface for parsing JSON.
 
 This is a very early release to see how cross-platform the underlying code is.
